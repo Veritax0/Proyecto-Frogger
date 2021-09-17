620d5ae53b0036dd733fe4d90c7f2ccacba3d2b4
MainTopita <carolina.melguizop@udea.edu.co>
2021-09-17 09:36:51 -0500

    clasesyget

--- javascript/Punto.js
+++ javascript/Punto.js
@@ -12,4 +12,71 @@ class Punto
         this.#vx = vx;
         this.#vy = vy;
     }
+
+    get x ()
+    {
+        return this.#x;
+    }
+
+    set x (value)
+    {
+        this.#x = value
+    }
+
+    get y ()
+    {
+        return this.#y;
+    }
+
+    set y (value)
+    {
+        this.#y = value;
+    }
+
+    get vx ()
+    {
+        return this.#vx;
+    }
+
+    set vx (value)
+    {
+        this.#vx = value;
+    }
+
+    get vy ()
+    {
+        return this.#vy;
+    }
+
+    set vy (value)
+    {
+        this.#vy = value;
+    }
+
+    mover ()
+    {
+        this.#y += this.#vy;
+        this.#x += this.#vx;
+    }
+
+    
+    moverArriba ()
+    {
+        this.#y += - 10;
+    }
+
+    moverAbajo ()
+    {
+        this.#y += 10;
+    }
+
+    moverIzquierda ()
+    {
+        this.#x += - 10;
+    }
+
+    moverDerecha ()
+    {
+        this.#x += 10;
+    }
 }
