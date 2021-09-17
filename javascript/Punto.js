8f8f1e27f7a0a4fb28f422bb28a7ae75a9690ff5
Veritax0 <juan.verao@udea.edu.co>
2021-09-17 09:03:03 -0500

    Clases

---
+++ javascript/Punto.js
@@ -0,0 +1,15 @@
+class Punto
+{
+    #x;
+    #y;
+    #vx;
+    #vy;
+
+    constructor(x,y,vx,vy)
+    {
+        this.#x = x;
+        this.#y = y;
+        this.#vx = vx;
+        this.#vy = vy;
+    }
+}
